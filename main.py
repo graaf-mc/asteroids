8c59c9e3fcec74000b991278bbcd5a25dcb48291
graaf-mc <aleijen@gmail.com>
2025-12-13 17:23:37 +0100

    Update fro Androids till Draw Palyer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 from logger import log_state
 
 def main():
@@ -8,6 +9,9 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         log_state()
@@ -15,7 +19,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
